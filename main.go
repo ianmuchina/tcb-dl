883b41bbf2d9346704080836236653157e58a9e1
Ian Muchina <noreply@ianmuchina.com>
2022-10-31 23:36:52 +0300

    modified:   main.go

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
-	yaml "gopkg.in/yaml.v3"
 )
 
+// Todo: Methods instead of functions
 type Project struct {
 	Title       string    `json:"Title"`
 	Description string    `json:"Description,omitempty"`
@@ -31,13 +31,20 @@ type Image struct {
 	Alt string
 }
 
+const base = "https://onepiecechapters.com"
+
 func main() {
+	// Download Latest Projects from the internet
 	Projects := updateProjects()
-	for i := range Projects {
-		Projects[i].Chapters = getProjectChapters(Projects[i].Url)
-		for j := range Projects[i].Chapters {
-			Projects[i].Chapters[j].Images = getChapterImages(Projects[i].Chapters[j].Url)
-			fmt.Println("Ch.", i, j)
+	// For each Project, Get all chapter Links and Images
+	// TODO: Concurrency
+	for p := range Projects {
+		Projects[p].Chapters = getProjectChapters(Projects[p].Url)
+		for c := range Projects[p].Chapters {
+			// Download Chapter Images
+			// TODO: Concurrency
+			Projects[p].Chapters[c].Images = getChapterImages(Projects[p].Chapters[c].Url)
+			fmt.Println("Prj:", p, "Ch.", c)
 		}
 	}
 	saveProjectsToDisk(Projects)
@@ -46,49 +53,34 @@ func main() {
 func updateProjects() []Project {
 	var Result []Project
 	// fetch latest chapters
-	res, err := http.Get("https://onepiecechapters.com/projects")
-	if err != nil {
-		log.Fatal(err, "Failed to get Latest Chapters")
-	}
-	defer res.Body.Close()
+	fetch("https://onepiecechapters.com/projects").
+		Find("div .bg-card").
+		Each(func(i int, s *goquery.Selection) {
+			var P Project
+			// Title
+			P.Title = s.Find(".font-bold").Text()
+			// Url
+			url, attrExists := s.Find("a").Attr("href")
+			if attrExists {
+				P.Url = url
+			} else {
+				log.Fatalln("Error getting Project Url")
+			}
 
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// find all projects using css selectors
-	doc.Find("div .bg-card").Each(func(i int, s *goquery.Selection) {
-		var P Project
-		// Title
-		P.Title = s.Find(".font-bold").Text()
-		// Url
-		url, attrExists := s.Find("a").Attr("href")
-		if attrExists {
-			P.Url = url
-		} else {
-			log.Fatalln("Error getting Project Url")
-		}
-
-		// Chapter Image
-		image := s.Find("img")
-		src, imageExists := image.Attr("src")
-		if imageExists {
-			P.Image = src
-		} else {
-			log.Fatalln("Error getting project image")
-		}
-		// Get Project Image & Description
-		// async
-		P.Image, P.Description = getProjectImageAndDescription(P.Url)
-		// Append Project
-		Result = append(Result, P)
-	})
+			// Chapter Image
+			image := s.Find("img")
+			src, imageExists := image.Attr("src")
+			if imageExists {
+				P.Image = src
+			} else {
+				log.Fatalln("Error getting project image")
+			}
+			// Get Project Image & Description
+			// async
+			P.Image, P.Description = getProjectImageAndDescription(P.Url)
+			// Append Project
+			Result = append(Result, P)
+		})
 
 	// wait
 	return Result
@@ -97,77 +89,75 @@ func updateProjects() []Project {
 func saveProjectsToDisk(Projects []Project) {
 	json, _ := json.MarshalIndent(Projects, "", "\t")
 	os.WriteFile("projects.json", json, 0644)
-	yaml, _ := yaml.Marshal(Projects)
-	os.WriteFile("projects.yml", yaml, 0644)
+	// yaml, _ := yaml.Marshal(Projects)
+	// os.WriteFile("projects.yml", yaml, 0644)
 }
 
 func getProjectImageAndDescription(url string) (string, string) {
-	url = "https://onepiecechapters.com" + url
-	description := ""
+	var description string = ""
 
 	// Fetch Project
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err, "Failed to get Latest Chapters")
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// find all projects using css selectors
-	doc.Find(".order-1").Each(func(i int, s *goquery.Selection) {
-		url = s.Find("img").AttrOr("src", "")
-		description = s.Find("p").Text()
-	})
+	fetch(base + url).
+		Find(".order-1").
+		Each(func(i int, s *goquery.Selection) {
+			url = s.Find("img").AttrOr("src", "")
+			description = s.Find("p").Text()
+		})
 
 	return url, description
 }
 
 // sync
 func getProjectChapters(url string) []Chapter {
-	Result := []Chapter{}
-	url = "https://onepiecechapters.com" + url
+	var Result []Chapter
 
-	// Fetch Project
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err, "Failed to get Latest Chapters")
-	}
-	defer res.Body.Close()
+	fetch(base + url).
+		Find("div .block.border.border-border.bg-card.mb-3.p-3 ").
+		// For Each Chapter
+		Each(func(i int, s *goquery.Selection) {
+			var C Chapter
+			// Get Chapter Permalink
+			val, exists := s.Attr("href")
+			if exists {
+				C.Url = val
+			} else {
+				log.Fatalln("Chapter Does not Exist")
+			}
+			// Get Chapter Text
+			C.Title = s.Find("div .text-lg.font-bold").Text()
+			// Get Chapter Description
+			C.Description = s.Find("div .text-gray-500").Text()
 
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Find all Chapters using css selectors
-	doc.Find("div .block.border.border-border.bg-card.mb-3.p-3 ").Each(func(i int, s *goquery.Selection) {
-		var C Chapter
-		C.Url = s.AttrOr("href", "")
-		C.Title = s.Find("div .text-lg.font-bold").Text()
-		C.Description = s.Find("div .text-gray-500").Text()
-		Result = append(Result, C)
-	})
+			Result = append(Result, C)
+		})
 	return Result
 }
 
+// Return array of image sources and their respective permalinks
 func getChapterImages(url string) []Image {
-	Result := []Image{}
-	url = "https://onepiecechapters.com" + url
+	var Result []Image
+	fetch(base + url).
+		Find("img.fixed-ratio-content").
+		Each(func(i int, s *goquery.Selection) {
+			var I Image
+			src, imageExists := s.Attr("src")
+			if imageExists {
+				I.Src = src
+			} else {
+				log.Fatal("image has no src attribute")
+			}
 
+			alt, altExists := s.Attr("alt")
+			// Alt is Optional
+			if altExists {
+				I.Alt = alt
+			}
+			Result = append(Result, I)
+		})
+	return Result
+}
+
+func fetch(url string) *goquery.Document {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err, "Failed to get Latest Chapters")
@@ -181,22 +171,6 @@ func getChapterImages(url string) []Image {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return doc
 
-	doc.Find("img.fixed-ratio-content").Each(func(i int, s *goquery.Selection) {
-		var I Image
-		src, imageExists := s.Attr("src")
-		if imageExists {
-			I.Src = src
-		} else {
-			log.Fatal("image has no src attribute")
-		}
-
-		alt, altExists := s.Attr("alt")
-		// Alt is Optional
-		if altExists {
-			I.Alt = alt
-		}
-		Result = append(Result, I)
-	})
-	return Result
 }
