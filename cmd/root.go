bb55b0793d4ca029a01ff1dd9b11812063fc3387
Ian Muchina <noreply@ianmuchina.com>
2022-11-17 18:36:00 +0300

    update

---
+++ cmd/root.go
@@ -0,0 +1,103 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ianmuchina/tcb-dl/lib"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tcb-dl",
+	Short: "Download Manga from tcbscans",
+	Long:  "",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+var downloadCmd = &cobra.Command{
+	Use:     "download",
+	Aliases: []string{"dl"},
+	Short:   "Download Chapter",
+	Run: func(cmd *cobra.Command, args []string) {
+		ch := cmd.Flag("chapter")
+		prj := cmd.Flag("project")
+
+		fmt.Println(ch.Value, prj.Value)
+	},
+}
+
+var downloadLatestCmd = &cobra.Command{
+	Use:     "latest",
+	Aliases: []string{"latest"},
+	Short:   "Download Latest Chapter",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Downloading latest")
+	},
+}
+
+var syncCmd = &cobra.Command{
+	Use:   "sync",
+	Short: "Sync chapter data",
+	Run: func(cmd *cobra.Command, args []string) {
+		lib.SyncNew()
+	},
+}
+
+var syncAll = &cobra.Command{
+	Use:   "all",
+	Short: "Refresh all chapter data",
+	Run: func(cmd *cobra.Command, args []string) {
+		lib.SyncAll()
+	},
+}
+
+var syncNew = &cobra.Command{
+	Use:   "new",
+	Short: "Fetch new Chapter data",
+	Run: func(cmd *cobra.Command, args []string) {
+		lib.SyncNew()
+		fmt.Println("Done")
+	},
+}
+
+// Flags
+var Chapter float32
+var Project uint8
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tcb-dl.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.AddCommand(downloadCmd)
+	rootCmd.AddCommand(syncCmd)
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Download
+	downloadCmd.Flags().Float32VarP(&Chapter, "chapter", "c", 0, "")
+	downloadCmd.Flags().Uint8VarP(&Project, "project", "p", 5, "")
+
+	downloadCmd.MarkFlagRequired("project")
+	downloadCmd.MarkFlagRequired("chapter")
+	downloadCmd.AddCommand(downloadLatestCmd)
+
+	syncCmd.AddCommand(syncAll, syncNew)
+}
---
+++ lib/lib.go
@@ -0,0 +1,419 @@
+package lib
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// Todo: Methods instead of functions
+type Project struct {
+	Title       string    `json:"Title"`
+	Description string    `json:"Description,omitempty"`
+	Url         string    `json:"Url"`
+	Image       string    `json:"Image,omitempty"`
+	Chapters    []Chapter `json:"Chapters,omitempty"`
+}
+
+type Chapter struct {
+	Index       string  `json:",omitempty"`
+	Title       string  `json:",omitempty"`
+	Url         string  `json:",omitempty"`
+	Description string  `json:",omitempty"`
+	Images      []Image `json:",omitempty"`
+}
+
+type Image struct {
+	Src string
+	Alt string
+}
+
+type CubariManga struct {
+	Title       string                   `json:"title"`
+	Description string                   `json:"description"`
+	Artist      string                   `json:"artist"`
+	Author      string                   `json:"author"`
+	Cover       string                   `json:"cover"`
+	Chapters    map[string]CubariChapter `json:"chapters"`
+}
+
+type CubariChapter struct {
+	Title        string              `json:"title"`
+	Volume       string              `json:"volume"`
+	Groups       map[string][]string `json:"groups"`
+	Last_updated string
+}
+
+const base = "https://onepiecechapters.com"
+
+var commit_msg string = ""
+
+// HTTP Client
+var httpClient = &http.Client{
+	Timeout: time.Second * 10,
+}
+
+var ChapterMap map[string]Chapter = make(map[string]Chapter, 4000)
+
+var Pmap = map[int]string{
+	1:  "/mangas/1/ace-novel-manga-adaptation",
+	2:  "/mangas/2/bleach",
+	3:  "/mangas/3/black-clover",
+	4:  "/mangas/4/jujutsu-kaisen",
+	5:  "/mangas/5/one-piece",
+	6:  "/mangas/6/my-hero-academia",
+	8:  "/mangas/8/attack-on-titan",
+	9:  "/mangas/9/build-king",
+	10: "/mangas/10/one-punch-man",
+	11: "/mangas/11/haikyu-special",
+	12: "/mangas/12/one-piece-nami-vs-kalifa-by-boichi",
+	13: "/mangas/13/chainsaw-man",
+	15: "/mangas/15/hunter-x-hunter",
+}
+
+func SyncAll() {
+	P := LoadLocalData()
+	UpdateChapterMap(P)
+
+	P = FetchAllChapters(P)
+	P = CleanData(P)
+
+	GenCubariData(P)
+	os.WriteFile("commit_msg", []byte(commit_msg), 0644)
+}
+
+func SyncNew() {
+	P := LoadLocalData()
+	UpdateChapterMap(P)
+
+	P = FetchNewChapters(P)
+	P = CleanData(P)
+
+	GenCubariData(P)
+	os.WriteFile("commit_msg", []byte(commit_msg), 0644)
+}
+
+func UpdateChapterMap(P []Project) {
+	for _, p := range P {
+		for _, c := range p.Chapters {
+			ChapterMap[c.Url] = c
+		}
+	}
+}
+
+func FetchNewChapters(P []Project) []Project {
+	Projects := UpdateProjects()
+
+	for i := 0; i < len(Projects); i++ {
+		Projects[i].Chapters = GetProjectChapters(Projects[i].Url)
+		for c := 0; c < len(Projects[i].Chapters); c++ {
+
+			ch := Projects[i].Chapters[c]
+			// If new chapter
+			if _, ok := ChapterMap[ch.Url]; !ok {
+				// fetch images
+				Projects[i].Chapters[c].Images = GetChapterImages(Projects[i].Chapters[c].Url)
+				fmt.Println("New", ch.Title)
+				commit_msg = commit_msg + ch.Title + "\n"
+			} else {
+				// Already seen
+				Projects[i].Chapters[c] = ChapterMap[ch.Url]
+			}
+		}
+	}
+
+	return Projects
+}
+
+func FetchAllChapters(P []Project) []Project {
+	Projects := UpdateProjects()
+
+	for i := 0; i < len(Projects); i++ {
+		Projects[i].Chapters = GetProjectChapters(Projects[i].Url)
+		for c := 0; c < len(Projects[i].Chapters); c++ {
+
+			ch := Projects[i].Chapters[c]
+			Projects[i].Chapters[c].Images = GetChapterImages(ch.Url)
+			fmt.Println("New", ch.Title)
+			commit_msg = commit_msg + ch.Title + "\n"
+		}
+	}
+
+	return Projects
+}
+
+func CleanData(Projects []Project) []Project {
+	re := regexp.MustCompile("[0-9.]+")
+
+	for i, p := range Projects {
+		for j, c := range p.Chapters {
+
+			x, err := strconv.ParseFloat(re.FindAllString(c.Title, -1)[0], 32)
+			if err != nil {
+				log.Fatal(err)
+			}
+			Projects[i].Chapters[j].Index = fmt.Sprintf("%g", x)
+		}
+	}
+
+	SaveProjectsToDisk(Projects)
+	return Projects
+}
+
+func LoadLocalData() []Project {
+	data, err := os.ReadFile("./projects.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp := []Project{}
+	err = json.Unmarshal(data, &tmp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tmp
+}
+
+func GenCubariData(P []Project) {
+	for _, m := range ExportCubariData(P) {
+		os.Mkdir("./data", 0755)
+		filename := "./data/" + m.Title + ".json"
+		filename = strings.ReplaceAll(filename, " ", "_")
+		json, err := json.MarshalIndent(m, "", "\t")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = os.WriteFile(filename, json, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func ExportCubariData(Projects []Project) []CubariManga {
+	var Result []CubariManga
+	for _, P := range Projects {
+		Manga := CubariManga{
+			Title:       P.Title,
+			Description: P.Description,
+			Artist:      "Unknown",
+			Author:      "Unknown",
+			Cover:       P.Image,
+			Chapters:    make(map[string]CubariChapter, len(P.Chapters)),
+		}
+
+		for _, C := range P.Chapters {
+			// Sort Chapter Images
+			var imgs []string
+			for _, I := range C.Images {
+				imgs = append(imgs, I.Src)
+			}
+			G := map[string][]string{
+				"tcbscans": imgs,
+			}
+
+			Chp := CubariChapter{
+				Title:  C.Title,
+				Groups: G,
+			}
+
+			Manga.Chapters[C.Index] = Chp
+		}
+
+		Result = append(Result, Manga)
+	}
+
+	return Result
+}
+
+func UpdateProjects() []Project {
+	var Result []Project
+	// fetch latest chapters
+	fetch("https://onepiecechapters.com/projects").
+		Find("div .bg-card").
+		Each(func(i int, s *goquery.Selection) {
+			var P Project
+			// Title
+			P.Title = s.Find(".font-bold").Text()
+			// Url
+			url, attrExists := s.Find("a").Attr("href")
+			if attrExists {
+				P.Url = url
+			} else {
+				log.Fatalln("Error Getting Project Url")
+			}
+
+			// Chapter Image
+			image := s.Find("img")
+			src, imageExists := image.Attr("src")
+			if imageExists {
+				P.Image = src
+			} else {
+				log.Fatalln("Error Getting project image")
+			}
+			// Get Project Image & Description
+			// async
+			P.Image, P.Description = GetProjectImageAndDescription(P.Url)
+			// Append Project
+			Result = append(Result, P)
+		})
+
+	// wait
+	return Result
+}
+
+func SaveProjectsToDisk(Projects []Project) {
+	json, _ := json.MarshalIndent(Projects, "", "\t")
+	os.WriteFile("projects.json", json, 0644)
+	// yaml, _ := yaml.Marshal(Projects)
+	// os.WriteFile("projects.yml", yaml, 0644)
+}
+
+func GetProjectImageAndDescription(url string) (string, string) {
+	var description string = ""
+	// Fetch Project
+	fetch(base + url).
+		Find(".order-1").
+		Each(func(i int, s *goquery.Selection) {
+			url = s.Find("img").AttrOr("src", "")
+			description = s.Find("p").Text()
+		})
+	return url, description
+}
+
+// sync
+func GetProjectChapters(url string) []Chapter {
+	var Result []Chapter
+
+	fetch(base + url).
+		Find("div .block.border.border-border.bg-card.mb-3.p-3 ").
+		// For Each Chapter
+		Each(func(i int, s *goquery.Selection) {
+			var C Chapter
+			// Get Chapter Permalink
+			val, exists := s.Attr("href")
+			if exists {
+				C.Url = val
+			} else {
+				log.Fatalln("Chapter Does not Exist")
+			}
+			// Get Chapter Uext
+			C.Title = s.Find("div .text-lg.font-bold").Text()
+			// Get Chapter Description
+			C.Description = s.Find("div .text-gray-500").Text()
+
+			Result = append(Result, C)
+		})
+	return Result
+}
+
+// Return array of image sources and their respective permalinks
+func GetChapterImages(url string) []Image {
+	var Result []Image
+	fetch(base + url).
+		Find("img.fixed-ratio-content").
+		Each(func(i int, s *goquery.Selection) {
+			var I Image
+			src, imageExists := s.Attr("src")
+			if imageExists {
+				I.Src = src
+			} else {
+				log.Fatal("image has no src attribute")
+			}
+
+			alt, altExists := s.Attr("alt")
+			// Alt is Optional
+			if altExists {
+				I.Alt = alt
+			}
+			Result = append(Result, I)
+		})
+	return Result
+}
+
+func fetch(url string) *goquery.Document {
+	res, err := httpClient.Get(url)
+	if err != nil {
+		log.Fatal(err, "Failed to Get Latest Chapters")
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		body, _ := io.ReadAll(res.Body)
+		res.Body.Close()
+		log.Fatalf("status code error: %d %s\n%s", res.StatusCode, res.Status, body)
+	}
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+
+}
+func DownloadChapter(prj int, ch string) {
+
+	P := LoadLocalData()
+	var x Project
+	for _, p := range P {
+		if p.Url == Pmap[prj] {
+			x = p
+		}
+	}
+
+	var max float64 = 0
+	var maxCh Chapter
+	// Get largest Chapter
+	for _, c := range x.Chapters {
+		cur, err := strconv.ParseFloat(c.Index, 32)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if cur > max {
+			max = cur
+			maxCh = c
+		}
+	}
+
+	return maxCh
+
+}
+
+func GetLatest(i int) Chapter {
+	P := LoadLocalData()
+	var x Project
+	for _, p := range P {
+		if p.Url == Pmap[i] {
+			x = p
+		}
+	}
+
+	var max float64 = 0
+	var maxCh Chapter
+	// Get largest Chapter
+	for _, c := range x.Chapters {
+		cur, err := strconv.ParseFloat(c.Index, 32)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if cur > max {
+			max = cur
+			maxCh = c
+		}
+	}
+
+	return maxCh
+}
